ecdd7b7c01fc3a6c952a30c4a39842ff246b561f
kisung <yoonki911@gmail.com>
2022-11-13 08:24:29 +0900

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by 윤기성 on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GithubDemoUITests/GithubDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GithubDemoUITestsLaunchTests.swift
+//  GithubDemoUITests
+//
+//  Created by 윤기성 on 2022/11/13.
+//
+
+import XCTest
+
+class GithubDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
